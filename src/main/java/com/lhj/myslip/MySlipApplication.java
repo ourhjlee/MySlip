2ee1baad5e114dec2063ca2573b8c14d8e07800d
hyukjoon <spades@naver.com>
2020-08-03 21:45:02 +0900

    최초 소스 커밋

---
+++ src/main/java/com/lhj/myslip/MySlipApplication.java
@@ -0,0 +1,13 @@
+package com.lhj.myslip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lhj/myslip/MySlipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhj.myslip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySlipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
